febecaf45d1c27e0eac56c2b86fe5cdc5f26559d
awyaki <awyakitaka@gmail.com>
2023-11-15 20:35:03 +0900

    Wait for rendering the App component to use msw on browser
    * See https://mswjs.io/docs/integrations/browser

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const enableMocking = async () => {
+  if (process.env.NODE_ENV != "development") return;
+
+  const { worker } = await import("./mocks/browser.ts");
+
+  return worker.start();
+};
+
+enableMocking()
+  .then(() => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    ReactDOM.createRoot(document.getElementById("root")!).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  })
+  .catch((e) => {
+    console.error(e);
+  });
