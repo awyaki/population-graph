7b7b8ed09ea6ce8bd26832a206e3fe512b23fd07
awyaki <awyakitaka@gmail.com>
2023-11-16 14:50:37 +0900

    RESUS API GET api/v1/population/composition/perYearのレスポンスに対するschemaを作成

--- api/scheme.ts
+++ api/scheme.ts
@@ -26,3 +26,56 @@ export const Prefectures = z.object({
       .array()
   ),
 });
+
+// [人口構成](https://opendata.resas-portal.go.jp/docs/api/v1/population/composition/perYear.html)
+
+const Data = z.discriminatedUnion("label", [
+  z.object({
+    label: z.literal("総人口"),
+    data: z
+      .object({
+        //labelが総人口のときに限りrateプロパティが存在しない
+        year: z.number(),
+        value: z.number(),
+      })
+      .array(),
+  }),
+  z.object({
+    label: z.literal("年少人口"),
+    data: z
+      .object({
+        year: z.number(),
+        value: z.number(),
+        rate: z.number(),
+      })
+      .array(),
+  }),
+  z.object({
+    label: z.literal("生産年齢人口"),
+    data: z
+      .object({
+        year: z.number(),
+        value: z.number(),
+        rate: z.number(),
+      })
+      .array(),
+  }),
+  z.object({
+    label: z.literal("老年人口"),
+    data: z
+      .object({
+        year: z.number(),
+        value: z.number(),
+        rate: z.number(),
+      })
+      .array(),
+  }),
+]);
+
+export const Population = z.object({
+  message: z.null(),
+  result: z.object({
+    boundaryYear: z.number(),
+    data: Data.array(),
+  }),
+});
