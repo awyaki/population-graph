e5dc58fee09d94e7a780b7508d0fcc8fde8d6d7f
awyaki <awyakitaka@gmail.com>
2023-11-17 10:10:29 +0900

    テスト実行確認用のコードを削除した

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
 import "./App.css";
-import { useQuery, QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-// When the app is running on development
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Prefectures />
+      <>Hello world</>
     </QueryClientProvider>
   );
 }
 
 export default App;
-
-export const usePrefectures = () => {
-  return useQuery("prefectures", fetchPrefectures);
-};
-
-const Prefectures: React.FC = () => {
-  const query = usePrefectures();
-  console.log("query data", query.data);
-  return <>Hello world</>;
-};
-
-const baseUrl = import.meta.env.MODE === "test" ? "http://localhost:3000" : "";
-
-const fetchPrefectures = async () => {
-  const res = await fetch(baseUrl + "/api/prefectures");
-  const data = (await res.json()) as unknown;
-  return data;
-};
