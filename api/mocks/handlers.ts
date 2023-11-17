237cbfd2a8818810a4e9ac1a950ed8c89ae25b33
awyaki <awyakitaka@gmail.com>
2023-11-17 09:47:40 +0900

    api/populationのテストモック用handlerを追加

--- api/mocks/handlers.ts
+++ api/mocks/handlers.ts
@@ -1,10 +1,37 @@
 import { http, HttpResponse } from "msw";
 import { prefectures } from "./data/prefectures.js";
+import { populationTokyo as tokyo } from "./data/population-13-tokyo.js";
+import { populationAichi as aichi } from "./data/population-23-aichi.js";
+import { populationOsaka as osaka } from "./data/population-27-osaka.js";
 
 export const handlers = [
   http.get(`/api/prefectures`, () => {
     console.log("Captured a GET /api/prefectures request");
 
     return HttpResponse.json(prefectures);
+  }),
+  http.get("/api/population", ({ request }) => {
+    const url = new URL(request.url);
+    const { searchParams } = url;
+
+    const prefCode = searchParams.get("prefCode");
+
+    if (prefCode === null) return new HttpResponse(null, { status: 404 });
+
+    console.log(`Captured a GET /api/population?prefCode=${prefCode} request`);
+
+    switch (prefCode) {
+      case "13": {
+        return HttpResponse.json(tokyo);
+      }
+      case "23": {
+        return HttpResponse.json(aichi);
+      }
+      case "27": {
+        return HttpResponse.json(osaka);
+      }
+      default:
+        return new HttpResponse(null, { status: 404 });
+    }
   }),
 ];
