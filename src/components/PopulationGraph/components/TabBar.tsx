0758ed68a46334b7601eb2bf16fbd9aeb667e23d
awyaki <awyakitaka@gmail.com>
2023-11-21 10:24:30 +0900

    TabBarにスタイルを追加

--- src/components/PopulationGraph/components/TabBar.tsx
+++ src/components/PopulationGraph/components/TabBar.tsx
@@ -1,7 +1,12 @@
+import styles from "./TabBar.module.css";
 type Props = {
   children: React.ReactNode;
 };
 
 export const TabBar: React.FC<Props> = ({ children }) => {
-  return <div role="tablist">{children}</div>;
+  return (
+    <div className={styles.wrapper} role="tablist">
+      <div className={styles.inner}>{children}</div>
+    </div>
+  );
 };
