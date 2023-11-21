bbd5ee49daa8cefad9554efcbd72795f5b96abcd
awyaki <awyakitaka@gmail.com>
2023-11-21 14:36:31 +0900

    typoの修正: RESUS -> RESAS

--- api/config.ts
+++ api/config.ts
@@ -2,4 +2,4 @@ import { env } from "./env.js";
 
 export const BASE_URL = "https://opendata.resas-portal.go.jp/api/v1";
 
-export const API_KEY = env.RESUS_API_KEY ?? "DUMMY_API_KEY";
+export const API_KEY = env.RESAS_API_KEY ?? "DUMMY_API_KEY";
