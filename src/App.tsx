c0239af4621a573f593721a38837a386754bf688
awyaki <awyakitaka@gmail.com>
2023-11-21 17:07:25 +0900

    AppContainerを使用しApp全体のpaddingを調整

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { usePrefecturesCheckBoxList } from "./hooks/usePrefecturesCheckBoxList";
 import PopulationGraph from "./components/PopulationGraph";
 import Responsive from "./layouts/TwoItemsResponsiveContainer";
+import Container from "./layouts/AppContainer";
 const queryClient = new QueryClient();
 
 function App() {
@@ -14,13 +15,15 @@ function App() {
     <QueryClientProvider client={queryClient}>
       {/* TODO: ErrorBoundaryを作る */}
       {/* TODO: Suspenseのフォールバックを作る */}
-      <h1>都道府県別人口構成グラフ</h1>
-      <Suspense fallback={<></>}>
-        <Responsive
-          first={renderPrefecturesCheckBoxList()}
-          second={<PopulationGraph checkedPrefs={checkedPrefs} />}
-        />
-      </Suspense>
+      <Container>
+        <h1>都道府県別人口構成グラフ</h1>
+        <Suspense fallback={<></>}>
+          <Responsive
+            first={renderPrefecturesCheckBoxList()}
+            second={<PopulationGraph checkedPrefs={checkedPrefs} />}
+          />
+        </Suspense>
+      </Container>
     </QueryClientProvider>
   );
 }
