69387429ee51a6f1504b1adb72a6d8b54b4822e7
awyaki <awyakitaka@gmail.com>
2023-11-21 16:56:03 +0900

    Appを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import "./App.css";
+import { Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { usePrefecturesCheckBoxList } from "./hooks/usePrefecturesCheckBoxList";
+import PopulationGraph from "./components/PopulationGraph";
+import Responsive from "./layouts/TwoItemsResponsiveContainer";
 const queryClient = new QueryClient();
 
 function App() {
+  const { checkedPrefs, renderPrefecturesCheckBoxList } =
+    usePrefecturesCheckBoxList();
+
   return (
     <QueryClientProvider client={queryClient}>
-      <>Hello world</>
+      {/* TODO: ErrorBoundaryを作る */}
+      {/* TODO: Suspenseのフォールバックを作る */}
+      <h1>都道府県別人口構成グラフ</h1>
+      <Suspense fallback={<></>}>
+        <Responsive
+          first={renderPrefecturesCheckBoxList()}
+          second={<PopulationGraph checkedPrefs={checkedPrefs} />}
+        />
+      </Suspense>
     </QueryClientProvider>
   );
 }
