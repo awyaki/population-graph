79d8c35ad948ae4b5521a2884e206f2fe2ae9b0e
awyaki <awyakitaka@gmail.com>
2023-11-21 19:33:46 +0900

    アプリ全体を囲むErrorBoundaryを設置

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PopulationGraphPage from "./pages/PopulationGraphPage";
+import ErrorPage from "./pages/ErrorPage";
+import { ErrorBoundary } from "react-error-boundary";
+
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <PopulationGraphPage />
-    </QueryClientProvider>
+    <ErrorBoundary fallback={<ErrorPage />}>
+      <QueryClientProvider client={queryClient}>
+        <PopulationGraphPage />
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
 
