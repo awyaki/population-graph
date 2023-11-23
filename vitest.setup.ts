d3ea3bef7220314b9e7e2c5dc88c6d37056e7f2a
awyaki <awyakitaka@gmail.com>
2023-11-23 18:00:36 +0900

    不要なコードを削除

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -3,15 +3,12 @@ import { server } from "./api/mocks/server";
 
 beforeAll(() => {
   server.listen();
-  console.log("before tests");
 });
 
 afterEach(() => {
   server.resetHandlers();
-  console.log("after tests");
 });
 
 afterAll(() => {
   server.close();
-  console.log("affter all tests");
 });
