3a16492733f33141db1223287e058466f0a2a7e9
awyaki <awyakitaka@gmail.com>
2023-11-23 21:10:57 +0900

    useCheckedPrefsWithNamesを作成

---
+++ src/pages/PopulationGraphPage/components/PopulationGraph/hooks/useCheckedPrefsWithNames.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+import { useMemo } from "react";
+import { Prefectures } from "../../../../../resas-api/schema";
+
+type Prefs = z.infer<typeof Prefectures>["result"];
+
+export const useCheckedPrefsWithNames = (
+  checkedPrefs: number[],
+  prefs: Prefs
+) => {
+  const checkedPrefsWithNames = useMemo(() => {
+    const prefsMap = new Map<number, string>(
+      prefs.map(({ prefCode, prefName }) => {
+        return [prefCode, prefName];
+      })
+    );
+
+    return checkedPrefs.map((prefCode) => {
+      const prefName = prefsMap.get(prefCode);
+
+      // checkedPrefsの要素の値はprefCodeである。
+      // prefsは全てのprefCodeにそれぞれ対応するオブジェクトを持つため
+      // prefsMap.get(prefCode)の値がundefinedになることはない
+      if (prefName === undefined) throw new Error("エラーが発生しました");
+
+      return { prefCode, prefName };
+    });
+  }, [checkedPrefs, prefs]);
+
+  return checkedPrefsWithNames;
+};
