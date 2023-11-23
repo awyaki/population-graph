420ed82c625261db5f0c8dec2c9aef829c8f932e
awyaki <awyakitaka@gmail.com>
2023-11-23 20:51:28 +0900

    useGraphからロジックを切り出しusePopulationを作成

---
+++ src/pages/PopulationGraphPage/components/PopulationGraph/hooks/usePopulation.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import { useSuspenseQueries } from "@tanstack/react-query";
+import { fetchPopulation } from "../../../../../resas-api/fetchPopulation";
+import { Population } from "../../../../../resas-api/schema";
+
+type Population = z.infer<typeof Population>["result"];
+
+export const usePopulation = (checkedPrefs: number[]): Population[] => {
+  const results = useSuspenseQueries({
+    queries: checkedPrefs.map((prefCode) => ({
+      queryKey: ["population", prefCode],
+      queryFn: () => fetchPopulation(prefCode),
+    })),
+  });
+
+  return results.map((v) => v.data);
+};
