983929f6556672c7f1db5457329a05ae46ad578d
awyaki <awyakitaka@gmail.com>
2025-04-11 10:57:08 +0900

    fix: add 'api/v1' to baseURL

--- api/config.ts
+++ api/config.ts
@@ -1,6 +1,6 @@
 import { env } from "./env.js";
 
 export const BASE_URL =
-  "https://yumemi-frontend-engineer-codecheck-api.vercel.app";
+  "https://yumemi-frontend-engineer-codecheck-api.vercel.app/api/v1";
 
 export const API_KEY = env.YUMEMI_API_KEY ?? "DUMMY_API_KEY";
