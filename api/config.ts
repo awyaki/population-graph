64f57597f3ca2070c9db3d3d9af38d397b452337
awyaki <awyakitaka@gmail.com>
2025-04-11 10:45:01 +0900

    change: change resas api to an api prepared by yumemi

--- api/config.ts
+++ api/config.ts
@@ -1,5 +1,6 @@
 import { env } from "./env.js";
 
-export const BASE_URL = "https://opendata.resas-portal.go.jp/api/v1";
+export const BASE_URL =
+  "https://yumemi-frontend-engineer-codecheck-api.vercel.app";
 
-export const API_KEY = env.RESAS_API_KEY ?? "DUMMY_API_KEY";
+export const API_KEY = env.YUMEMI_API_KEY ?? "DUMMY_API_KEY";
--- api/env.ts
+++ api/env.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const Env = z.object({
-  RESAS_API_KEY: z.string().optional(),
+  YUMEMI_API_KEY: z.string().optional(),
 });
 
 export const env = Env.parse(process.env);
